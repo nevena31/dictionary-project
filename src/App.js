6f2cf5fab031709038d7d83220c33813f91275bf
nevena31 <125161542+nevena31@users.noreply.github.com>
2023-12-26 20:01:19 +0100

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import logo from "./logo.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
     </div>
   );
 }
